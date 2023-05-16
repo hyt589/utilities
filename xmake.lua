11a2a20353dbc08bccb2330233f7cebfb4692501
Yutong Huang <yutong.huang@smartmore.com>
2023-05-16 09:49:39 +0800

    fix: xmake build

--- xmake.lua
+++ xmake.lua
@@ -5,20 +5,20 @@ local target_name = "hyt_common"
 
 target(target_name)
     if hyt_build_shared then
-        print("building hyt as shared")
         set_kind("shared")
         add_defines("HYT_EXPORT", "HYT_BUILD_SHARED")
     else
-        print("building hyt as static")
         set_kind("static")
     end
-    set_kind("shared")
     add_files("src/logging/*.cpp")
     add_files("src/scheduling/*.cpp")
     add_includedirs("include", {public = true})
 
-target("demo")
-    set_kind("binary")
-    add_files("demo/*.cpp")
-    add_deps(target_name)
+if hyt_build_demo then
+    target("demo")
+        set_kind("binary")
+        add_files("demo/*.cpp")
+        add_deps(target_name)
+end
+
 
